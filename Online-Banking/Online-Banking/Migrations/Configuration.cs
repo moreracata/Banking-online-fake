e80fc034f8455d093c45e252ecf69ac1429e1a23
Catalina Morera <moreracata@gmail.com>
2018-08-04 12:06:19 -0600

    First Commit
    Create empty solution

---
+++ Online-Banking/Online-Banking/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace Online_Banking.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Online_Banking.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Online_Banking.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ Online-Banking/Online-Banking/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Online_Banking.Startup))]
+namespace Online_Banking
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
